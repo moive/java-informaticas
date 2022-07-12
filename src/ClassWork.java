bf1c300fb952d7380b53e69a709f006acaca6aee
Moises Velasquez <mvelasquez@pictoric.com>
2022-07-11 23:53:31 -0500

    input output number

--- src/ClassWork.java
+++ src/ClassWork.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class ClassWork {
 
 	public static void main(String[] args) {
-		inputOuputData();
+		//inputOuputData();
+		//inputOutputJOptionPane();
+		inputOutputNumber();
 	}
 	
 	public static void inputOuputData() {
@@ -20,6 +24,26 @@ public class ClassWork {
 		
 		System.out.println("Hi " + user_name + ". The next year you will be " + (age + 1) + " years old");
 		
+	}
+	
+	public static void inputOutputJOptionPane() {
+		String username = JOptionPane.showInputDialog("Enter name");
+		
+		String strAge = JOptionPane.showInputDialog("Enter age");
+		
+		int age = Integer.parseInt(strAge);
+		age ++;
+		
+		System.out.println("Hello, " + username + ". Next year you will be " + age + " years old.");
+	}
+	
+	public static void inputOutputNumber() {
+		String strNumber = JOptionPane.showInputDialog("Enter a number");
+		
+		double numb = Double.parseDouble(strNumber);
+		
+		System.out.print("Sqrt of " + numb + " is ");
+		System.out.printf("%1.4f", Math.sqrt(numb));
 	}
 
 }
