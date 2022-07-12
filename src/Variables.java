c3de628ebafe93283c6731eb1aaea169153410bc
Moises Velasquez <mvelasquez@pictoric.com>
2022-07-11 19:45:18 -0500

    working with variables and working class Math

--- src/Variables.java
+++ src/Variables.java
@@ -3,7 +3,9 @@ public class Variables {
 	public static void main(String[] args) {
 		//Variables.getAmount();
 		//Variables.convertCentimetersToInches();
-		Variables.mathSqrt();
+		//Variables.mathSqrt();
+		
+		mathRound();
 	}
 	
 	private static void getAmount() {
@@ -28,5 +30,15 @@ public class Variables {
 	private static void mathSqrt() {
 		double sqrt = Math.sqrt(9);
 		System.out.println(sqrt);
+	}
+	
+	private static void mathRound() {
+		float num1 = 5.45f;		
+		int result = Math.round(num1);
+		System.out.println("The num1 rounded is: " + result);
+		
+		final double PI = Math.PI;
+		int roundPI = (int)Math.round(PI);
+		System.out.println("The number PI rounded is: " + roundPI);
 	}
 }
