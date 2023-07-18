474972adf65932d9b12711243656cb340b1e9a4b
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-07-17 20:51:52 -0500

    init POO

---
+++ src/poo/Car.java
@@ -0,0 +1,17 @@
+package poo;
+
+public class Car {
+	int wheels;
+	int large;
+	int width;
+	int engine;
+	int weigth;
+	
+	public Car() {
+		wheels = 4;
+		large = 2000;
+		width = 300;
+		engine = 1600;
+		weigth = 500;
+	}
+}
---
+++ src/poo/CarUse.java
@@ -0,0 +1,11 @@
+package poo;
+
+public class CarUse {
+
+	public static void main(String[] args) {
+		Car renault = new Car(); // Instancia una clase. Ejemplar de clase
+		
+		System.out.println("This car has " + renault.wheels + " wheels.");
+	}
+
+}
