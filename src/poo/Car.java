6e77307be0aadc70844452a399de31b42ffc3d4b
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-07-17 22:02:39 -0500

    modularization and encapsulation

--- src/poo/Car.java
+++ src/poo/Car.java
@@ -1,11 +1,14 @@
 package poo;
 
 public class Car {
-	int wheels;
-	int large;
-	int width;
-	int engine;
-	int weigth;
+	private int wheels;
+	private int large;
+	private int width;
+	private int engine;
+	private int weigth;
+	String color;
+	int totalWeigth;
+	boolean leatherSeats, climateControl;
 	
 	public Car() {
 		wheels = 4;
@@ -13,5 +16,17 @@ public class Car {
 		width = 300;
 		engine = 1600;
 		weigth = 500;
+	}
+	
+	public String getLarge() {
+		return "The large of the car is " + large;
+	}
+	
+	public void set_color() {
+		color = "blue";
+	}
+	
+	public String getColor() {
+		return "The color of the car is: " + color;
 	}
 }
--- src/poo/CarUse.java
+++ src/poo/CarUse.java
@@ -4,8 +4,9 @@ public class CarUse {
 
 	public static void main(String[] args) {
 		Car renault = new Car(); // Instancia una clase. Ejemplar de clase
+		renault.set_color();
 		
-		System.out.println("This car has " + renault.wheels + " wheels.");
+		System.out.println(renault.getColor());
 	}
 
 }
